5179c1487bc8046d8504303e4550135e8bfdd2d1
Ngoni <ngonimapfumo@hotmail.com>
2024-10-11 22:02:02 +0200

    Initial Commit

---
+++ Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playground
+//
+//  Created by Ngoni Mapfumo on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Ngoni Mapfumo on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
