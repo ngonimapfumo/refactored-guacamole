5ccafbcf81e20345f1c870908508648955c4da3c
Ngoni <ngonimapfumo@hotmail.com>
2024-10-12 02:37:24 +0200

    initial commit

--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fieldPlaceHolder=""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Image(.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300, alignment: .center)
+            Text("Welcome to Bla Bla!")
+            Text("Please Sign in to proceed")
+            
+            Form{
+                TextField(
+                    "username",text:$fieldPlaceHolder)
+                .font(.caption2)
+                .frame(width:300, height:46, alignment: .leading)
+                
+                TextField(
+                    "password",text:$fieldPlaceHolder)
+                .font(.caption2)
+                .frame(width:300, height:46, alignment: .leading)
+            }.frame(height: 200)
+            
+            
+            
+            
+            
+            Button(action:{
+                
+            }){
+                Text("Login")
+            }
+            
+            Button(action:{
+                
+            }){
+                Text("Sign Up")
+            }
+            
+            Spacer()
+            
+            Text("Forgot Your password?")
+                
+            
+            
+        }.padding()
+        
     }
 }
 
